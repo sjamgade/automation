c2b1bd203952372f291b460c118d7b3690c0847c
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-11-11 14:28:51 +0000

    gatehost: setup bond and bridge interfaces for VM

--- scripts/gatehost/setup.sh
+++ scripts/gatehost/setup.sh
@@ -7,6 +7,9 @@ rcntpd restart
 # set secure root password
 sed -i -e 's#^root:.*#root:$6$oh/u8h6j$876vgM2dJsuwRtfzlf6JlwYkxlY64jGKL5KFYqR51MLQLaVHlJ.V7ESn9OWlVbcNagSR.P4ON6uSONs60.iYv0:17116::::::#' /etc/shadow
 
+cp -a network/* /etc/sysconfig/network/
+rcnetwork restart
+
 # install media is not needed when we have the Pool repo
 zypper rr SLES12-SP2-12.2-0
 zypper --non-interactive in --no-recommends libvirt qemu-kvm
