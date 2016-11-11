1a698ace1e67d8ab061971c1fb68e9d7976c84e4
Bernhard M. Wiedemann <bwiedemann@suse.de>
2016-11-11 15:10:21 +0100

    gatehost: general config

--- scripts/gatehost/setup.sh
+++ scripts/gatehost/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+git config --global push.default simple
+echo server ntp1.suse.de iburst minpoll 4 >> /etc/ntp.conf 
+echo server ntp2.suse.de iburst >> /etc/ntp.conf
+chkconfig ntpd on
+rcntpd restart
+# set secure root password
+sed -i -e 's#^root:.*#root:$6$oh/u8h6j$876vgM2dJsuwRtfzlf6JlwYkxlY64jGKL5KFYqR51MLQLaVHlJ.V7ESn9OWlVbcNagSR.P4ON6uSONs60.iYv0:17116::::::#' /etc/shadow
+
 # install media is not needed when we have the Pool repo
 zypper rr SLES12-SP2-12.2-0
 zypper --non-interactive in --no-recommends libvirt qemu-kvm
