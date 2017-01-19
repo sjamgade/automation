b9999a44fa800bd1be3e2b0ed5869d223716be9e
Bernhard M. Wiedemann <bwiedemann@suse.de>
2017-01-19 15:08:51 +0100

    gatehost: cleanup

--- scripts/gatehost/setup.sh
+++ scripts/gatehost/setup.sh
@@ -4,8 +4,6 @@ using salt-ssh gatehost state.highstate'
 
 grep -q salt /etc/motd || { echo "error: salt not applied" ; exit 74; }
 
-git config --global push.default simple
-
 pushd /tmp
 wget http://clouddata.cloud.suse.de/images/x86_64/SLES12-SP2.qcow2
 qemu-img convert SLES12-SP2.qcow2 /dev/gate/gatevm
@@ -24,5 +22,5 @@ echo "default root password is linux"
 ssh gatevm mkdir -p ~/.ssh/
 scp ~/.ssh/authorized_keys gatevm:.ssh/
 scp -r /etc/zypp/repos.d gatevm:/etc/zypp/
-ssh gatevm zypper -n in python-pyOpenSSL python-xml
+ssh gatevm zypper -n in python-pyOpenSSL python-xml # for salt to work
 echo "now use salt-ssh gatevm state.highstate"
